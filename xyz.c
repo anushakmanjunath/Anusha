a6ada69dc78cd3991e5067061afefe0eacd32244
anushakmanjunath <49462964+anushakmanjunath@users.noreply.github.com>
2019-06-06 15:11:17 +0530

    Update xyz.c

--- xyz.c
+++ xyz.c
@@ -1,33 +1,33 @@
 #include<stdio.h>
 
 
-void getinput(int c,int d)
+void getinput(int *c,int *d)
 {
 	int a,b;
 	printf("enter 2 nos");
 	scanf("%d%d",&a,&b);
-         c=a;
-          d=b;
+         *c=a;
+          *d=b;
 
 }
-int  add(int a,int b)
+int  add(int *a,int *b)
 {
 		int c;
-        c=a+b;
+        c=(*a)+(*b);
 
 	return c;
 
 }
 void output(int c)
 {
-    printf("c= %d",c);	
+    printf("c= %d", c);	
 }
 
 int main()
 {
-		int c,d,e;
-   getinput(c,d);
-   add(c,d);
+		int c=0,d=0,e=0;
+   getinput(&c,&d);
+   e=add(&c,&d);
   output(e);
 return 0;
 
