9eb426ae9723c6d825bb39eeca863134f667e98d
anushakmanjunath <49462964+anushakmanjunath@users.noreply.github.com>
2019-06-06 14:38:22 +0530

    Update xyz.c
    i m getting the output but i m not getting the right  answer???

--- xyz.c
+++ xyz.c
@@ -4,19 +4,26 @@
 void getinput(int c,int d)
 {
 	int a,b;
+	printf("enter 2 nos");#include<stdio.h>
+
+
+void getinput(int c,int d)
+{
+	int a,b;
 	printf("enter 2 nos");
 	scanf("%d%d",&a,&b);
-c=a;
-d=b
-	add(c,d)
+         c=a;
+          d=b;
+	add(c,d);
 }
 int  add(int a,int b)
 {
 		int c=a+b;
+
 	return c;
 
 }
-void output(int c)
+void output(int *c)
 {
     printf("c= %d");	
 }
@@ -26,6 +33,33 @@ int main()
 		int c,d;
    getinput(c,d);
    add(c,d);
+  output(&c);
+return 0;
+
+}
+	scanf("%d%d",&a,&b);
+         c=a;
+          d=b;
+	add(c,d);
+}
+int  add(int a,int b)
+{
+		int c=a+b;
+
+	return c;
+
+}
+void output(int *c)
+{
+    printf("c= %d");	
+}
+
+int main()
+{
+		int c,d;
+   getinput(c,d);
+   add(c,d);
+  output(&c);
 return 0;
 
 }
