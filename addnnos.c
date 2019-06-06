7d835781ec0af0c32f093e8eba26d004b673e904
anushakmanjunath <49462964+anushakmanjunath@users.noreply.github.com>
2019-06-06 15:42:26 +0530

    Update addnnos.c

--- addnnos.c
+++ addnnos.c
@@ -1,1 +1,43 @@
 
+#include<stdio.h>
+
+
+void getinput(int c[],int n)
+{
+	int i;
+	printf("enter %d nos",n);
+	for(i=0;i<n;i++)
+	{	
+		scanf("%d",&c[i]);
+         }
+
+}
+int  add(int c[],int n)
+{
+	int i,sum;
+	for(i=0;i<n;i++)
+	{
+		sum=c[i]+c[i+1];
+	}
+	return sum;
+
+}
+void output(int c)
+{
+	printf("sum=%d",c);
+  
+}
+
+int main()
+{
+		int n,
+    int e;
+    int a[n];
+   printf("Enter the value of N:");
+   scanf("%d",&n);
+   getinput(a[],n);
+   e=add(a[],n)
+   output(e);
+  return 0;
+
+}
